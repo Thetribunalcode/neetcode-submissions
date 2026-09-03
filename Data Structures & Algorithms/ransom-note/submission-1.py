3fa74fe37263be5623af88026b4ab6888dc7cdc8
Ullas Rajesh <79746472+Thetribunalcode@users.noreply.github.com>
2026-09-04 00:15:37 +0530

    Add: ransom-note - submission-1

---
+++ Data Structures & Algorithms/ransom-note/submission-1.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magazine = list(magazine)
+
+        for c in ransomNote:
+            if c not in magazine:
+                return False
+            else:
+                magazine.remove(c)
+
+        return True
